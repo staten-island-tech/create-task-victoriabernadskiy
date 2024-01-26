035688243867521c41947f8993eb4d1f26c70b7a
victoriabernadskiy <vbernadskiy0531@gmail.com>
2024-01-26 00:30:48 -0500

    fhd

--- main.js
+++ main.js
@@ -1,42 +1,11 @@
-/* const URL = `https:///api/fruit/all`;
-
-
-async function getData(URL){
-try{
-const response = await fetch(URL);
-if(response.status !=200) {
-    throw new Error(response.statusText);
+const config = {
+API_KEY: "ix0SiU5PpY8EPBlzWE3K72qFsTWypdZpP0VKgC2o"
 }
-
-console.log(response)
-const data = await response.json();
-console.log(data);
-} catch (error) {}
-document.querySelector("h1").textContent = "error";
-}
-
-getData(URL); */
-
-//or
-
-/* 
-const url = `https://217.160.142.194.api/fruit/all`;
-
-async function fetchAsync (url) {
-    let response = await fetch(url);
-    let data = await response.json();
-    return data;
-  }
-console.log(url) */
+const url = 'https://api.nal.usda.gov/fdc/v1/foods/list?api_key='
+const api_key = config.API_KEY
 
 
-
-
-
-// use GET
-
-  const URL = `https://www.fruityvice.com/api/fruit/all`;
-    const FetchURL = async (request) => {
+const FetchURL = async (request) => {
   try {
     const response = await fetch(`${request}`)
     const data = await response.json()
@@ -47,7 +16,14 @@ console.log(url) */
   }
 }
 
-FetchURL()
+async function load() {
+  const nasadata = await FetchURL(`${url}${api_key}`)
+  Insert(nasadata)
+}
+
+load()
+
+
 
 const plate = []
 
